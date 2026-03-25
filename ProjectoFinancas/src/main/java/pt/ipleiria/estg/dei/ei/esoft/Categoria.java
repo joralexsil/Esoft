faf43751ea786bf32696f2f352d6155693194f48
Jorge Silva <2241986@my.ipleiria.pt>
2026-03-25 22:05:55 +0000

    2nd commit

---
+++ ProjectoFinancas/src/main/java/pt/ipleiria/estg/dei/ei/esoft/Categoria.java
@@ -0,0 +1,17 @@
+package pt.ipleiria.estg.dei.ei.esoft;
+
+public enum Categoria {
+    PUBLICIDADE,
+    DIVIDAS,
+    BENEFICIOS,
+    CONSUMIVEIS,
+    CORREIO,
+    RENDA,
+    HIPOTECA,
+    IMPOSTOS,
+    SERVICOS,
+    PUBLICOS,
+    SEGUROS,
+    JUROS,
+    OUTRO
+}
