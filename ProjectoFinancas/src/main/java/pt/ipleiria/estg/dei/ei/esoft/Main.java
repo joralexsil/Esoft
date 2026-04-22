20d8af219bb39943dd0f043e399d9d6a57007ab7
João Santana <joao.gameiro.santana@gmail.com>
2026-04-22 22:25:00 +0100

    Ready to go

--- ProjectoFinancas/src/main/java/pt/ipleiria/estg/dei/ei/esoft/Main.java
+++ ProjectoFinancas/src/main/java/pt/ipleiria/estg/dei/ei/esoft/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int opcao, id = 1;
+        int opcao, idTotal = 1;
         GestorDespesa gestor = new GestorDespesa();
 
         do {
@@ -13,27 +13,32 @@ public class Main {
             System.out.println("1. Adicionar despesa");
             System.out.println("2. Listar todas as despesas");
             System.out.println("3. Editar despesa");
+            System.out.println("4. Exportar despesas (PDF)");
             System.out.println("0. Sair");
             System.out.print("Opção: ");
             opcao = sc.nextInt();
 
             switch (opcao) {
                 case 1 -> {
+
+                    int dia,mes,ano,id = 0;
+                    id = idTotal + 1;
+
                     Despesa Novadespesa = new Despesa(id);
                     SeletorDatas novadata;
-                    int dia,mes,ano;
-
                     System.out.print("Nome da despesa: ");
-                    Novadespesa.descricao = sc.next();
+                    Novadespesa.setDescricao(sc.next());
 
                     System.out.print("Valor(€): ");
-                    Novadespesa.valor = sc.nextDouble();
+                    Novadespesa.setValor(sc.nextDouble());
 
                     System.out.println("Vamos Inserir a Data!");
                     System.out.print("Dia: ");
                     dia = sc.nextInt();
+
                     System.out.print("Mes: ");
                     mes = sc.nextInt();
+
                     System.out.print("Ano: ");
                     ano = sc.nextInt();
 
@@ -41,26 +46,31 @@ public class Main {
                     Novadespesa.setData(novadata);
                     gestor.registar(Novadespesa);
                 }
-                case 2 -> {return;}
+                case 2 -> {
+                    gestor.listar();
+                }
                 case 3 ->{
+
+                    int dia,mes,ano, id;
                     Despesa Novadespesa = new Despesa(0);
                     SeletorDatas novadata;
-                    int dia,mes,ano, id;
 
                     System.out.print("ID da Despesa a alterar: ");
                     id = sc.nextInt();
 
                     System.out.print("Nome da despesa: ");
-                    Novadespesa.descricao = sc.next();
+                    Novadespesa.setDescricao(sc.next());
 
                     System.out.print("Valor(€): ");
-                    Novadespesa.valor = sc.nextDouble();
+                    Novadespesa.setValor(sc.nextDouble());
 
                     System.out.println("Vamos Inserir a Data!");
                     System.out.print("Dia: ");
                     dia = sc.nextInt();
+
                     System.out.print("Mes: ");
                     mes = sc.nextInt();
+
                     System.out.print("Ano: ");
                     ano = sc.nextInt();
 
@@ -70,12 +80,14 @@ public class Main {
 
                     System.out.println("Despesa alterada com sucesso!");
                 }
+                case 4 -> {
+                    gestor.exportarPDF();
+                }
                 case 0 -> System.out.println("A sair...");
                 default -> System.out.println("Opção inválida!");
                 //
             }
         } while (opcao != 0);
-
         sc.close();
     }
 }
